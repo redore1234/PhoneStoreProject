0726c08b68e499adfc73fdbe683f8d89a161e353
Long Phạm <longptse140136@fpt.edu.vn>
2020-10-29 22:44:45 +0700

    Finish TblEmployeeDAO, Add References AutoMapper, Add Model(notyet) and IModel(notyet)

---
+++ ManagePhone/Models/Model.cs
@@ -0,0 +1,33 @@
+﻿using ActionService;
+using AutoMapper;
+using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagePhone.Models
+{
+    public class Model : IModel
+    {
+        static Service service = new Service();
+
+        static Model()
+        {
+            Mapper.CreateMap<TblEmployee, EmployeeModel>();
+        }
+
+        #region Login / Logout
+        public bool Login(string username, string password)
+        {
+            return true;
+        }
+
+        public void Logout()
+        {
+
+        }
+        #endregion
+    }
+}
