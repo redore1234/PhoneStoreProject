67a74def12a5e91388961144df6b8230f0eadf7d
dtin <damdongtin@gmail.com>
2020-10-30 14:24:27 +0700

    Change SQL data-type Tables (tblRole, tblStatus, tblRank, tblBrand)

--- PhoneStore.sql
+++ PhoneStore.sql
@@ -2,22 +2,22 @@ CREATE DATABASE PhoneStore
 USE PhoneStore
 
 CREATE TABLE tblRole(
-    roleID VARCHAR(10) PRIMARY KEY,
+    roleID INT IDENTITY(1,1) PRIMARY KEY,
     roleName NVARCHAR(30) NOT NULL,
 )
 
 CREATE TABLE tblStatus(
-    statusID VARCHAR(10) PRIMARY KEY,
+    statusID INT IDENTITY(1,1) PRIMARY KEY,
     statusName NVARCHAR(30) NOT NULL,
 )
 
 CREATE TABLE tblRank(
-    rankID VARCHAR(10) PRIMARY KEY,
+    rankID INT IDENTITY(1,1) PRIMARY KEY,
     rankName NVARCHAR(30) NOT NULL, 
 )
 
 CREATE TABLE tblBrand(
-    brandID VARCHAR(10) PRIMARY KEY,
+    brandID INT IDENTITY(1,1) PRIMARY KEY,
     brandName NVARCHAR(50) NOT NULL
 )
 
@@ -28,32 +28,32 @@ CREATE TABLE tblEmployee(
     DOB DATE NOT NULL,
     address NVARCHAR(100),
     phone VARCHAR(10),
-    roleID VARCHAR(10) FOREIGN KEY REFERENCES tblRole(roleID),
-    statusID VARCHAR(10) FOREIGN KEY REFERENCES tblStatus(statusID)
+    roleID INT FOREIGN KEY REFERENCES tblRole(roleID),
+    statusID INT FOREIGN KEY REFERENCES tblStatus(statusID)
 )
 
 CREATE TABLE tblCustomer(
-    customerID int IDENTITY(1,1) PRIMARY KEY,
+    customerID INT IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     DOB DATE NOT NULL,
     address NVARCHAR(100),
     phone VARCHAR(10),
     spentMoney BIGINT DEFAULT 0 NOT NULL, 
     point INT DEFAULT 0 NOT NULL,
-    rankID VARCHAR(10) FOREIGN KEY REFERENCES tblRank(rankID),
-    statusID VARCHAR(10) FOREIGN KEY REFERENCES tblStatus(statusID)
+    rankID INT FOREIGN KEY REFERENCES tblRank(rankID),
+    statusID INT FOREIGN KEY REFERENCES tblStatus(statusID)
 )
 
 CREATE TABLE tblProducts(
     productID INT IDENTITY(1,1) PRIMARY KEY,
     prodcutName NVARCHAR(50) NOT NULL,
-    brandID VARCHAR(10) FOREIGN KEY REFERENCES tblBrand(brandID),
+    brandID INT FOREIGN KEY REFERENCES tblBrand(brandID),
     description NVARCHAR(200) NOT NULL,
     launchDate DATE NOT NULL,
     price BIGINT DEFAULT 0 NOT NULL,
     image VARCHAR(200) NOT NULL,
     quantity INT DEFAULT 0 NOT NULL,
-    statusID VARCHAR(10) FOREIGN KEY REFERENCES tblStatus(statusID)
+    statusID INT FOREIGN KEY REFERENCES tblStatus(statusID)
 )
 
 CREATE TABLE tblOrders(
