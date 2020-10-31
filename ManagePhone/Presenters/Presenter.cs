7a21785de22bac5ee3146baeacdb3665a99b166a
longlb88 <longlebao2000@gmail.com>
2020-10-31 10:56:54 +0700

    Add Presenter.cs

--- ManagePhone/Presenters/Presenter.cs
+++ ManagePhone/Presenters/Presenter.cs
@@ -1,4 +1,5 @@
-﻿using ManagePhone.Views;
+﻿using ManagePhone.Models;
+using ManagePhone.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,17 @@ namespace ManagePhone.Presenters
 {
     public class Presenter<T> where T : IView
     {
+        protected static IModel Model { get; set; }
+        protected T View { get; set; }
+
+        static Presenter()
+        {
+            Model = new Model();
+        }
+
+        public Presenter(T view)
+        {
+            View = view;
+        }
     }
 }
